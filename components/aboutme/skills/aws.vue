e12930101e5fdbc902242e221ee7ef69ce767761
mattrlee35 <mattrlee35@gmail.com>
2023-08-14 19:08:29 -0500

    updated AWS icon

--- components/aboutme/skills/aws.vue
+++ components/aboutme/skills/aws.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- <img class="w-28 h-28 sm:w-28 sm:h-28 rounded-full bg-darkerblue border-2 border-golden" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"> -->
     <div class="relative group">
-        <img class="inline-block w-28 h-28 sm:w-28 sm:h-28 rounded-full object-fill bg-darkerblue border-2 border-golden" src="https://static-00.iconduck.com/assets.00/aws-icon-1024x1024-runl182z.png" alt="AWS"/>
+        <img class="inline-block w-28 h-28 sm:w-28 sm:h-28 rounded-full object-fill bg-darkerblue border-2 border-golden" src="https://i.imgur.com/IABPuCS.png" alt="AWS"/>
         <div class="absolute inset-0 flex text-center items-center justify-center bg-darkerblue break-keep border-2 border-golden bg-opacity-100 text-xl text-golden font-bold opacity-0 group-hover:opacity-100 transition-opacity rounded-full break-all">
             AWS
         </div>
