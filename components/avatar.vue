a232c6c91a77003ef56bf7e75add4589a1d182c8
mattrlee35 <mattrlee35@gmail.com>
2023-09-22 16:23:03 -0500

    updated avatar fully

--- components/avatar.vue
+++ components/avatar.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="https://i.imgur.com/S1npVgh.png" class="align-middle -m-16 -ml-20 lg:-ml-16 max-w-[300px] border-4 bg-darkerblue border-golden border-solid rounded-full"/>
+    <img src="https://i.imgur.com/y6nipxG.png" class="align-middle h-45 -m-16 -ml-20 lg:-ml-16 max-w-[300px] border-4 bg-darkerblue border-golden border-solid rounded-full"/>
 </template>
