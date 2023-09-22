fd798e45b1f225000e279a63cf8e425967f25179
mattrlee35 <mattrlee35@gmail.com>
2023-09-22 16:12:55 -0500

    updated avatar

--- components/avatar.vue
+++ components/avatar.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="https://i.imgur.com/1z5i44F.png" class="align-middle -m-16 -ml-20 lg:-ml-16 max-w-[300px] border-4 bg-darkerblue border-golden border-solid rounded-full"/>
+    <img src="https://i.imgur.com/S1npVgh.png" class="align-middle -m-16 -ml-20 lg:-ml-16 max-w-[300px] border-4 bg-darkerblue border-golden border-solid rounded-full"/>
 </template>
